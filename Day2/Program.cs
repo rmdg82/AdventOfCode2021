b95b07fbe002d195931acf34ef90868f85d63e3f
Romeo di Gregorio <r.digregorio@crif.com>
2021-12-10 17:24:14 +0100

    Solve Day2 problems

---
+++ Day2/Program.cs
@@ -0,0 +1,105 @@
+﻿using Shared.Helpers;
+
+namespace Day2;
+
+public class Program
+{
+    private const string _inputFileName = "input1.txt";
+    private const string _testFileName = "testInput.txt";
+
+    public static void Main(string[] args)
+    {
+        ConsoleHelper.PrintHeader(dayNum: 2);
+
+        ShowResultPart1(GetInputLines(_inputFileName));
+        ShowResultPart2(GetInputLines(_inputFileName));
+
+        ConsoleHelper.Exit();
+    }
+
+    private static void ShowResultPart1(List<string> lines)
+    {
+        int horizontalPosition = 0;
+        int depth = 0;
+
+        foreach (var line in lines)
+        {
+            var direction = GetDirection(line);
+            var value = GetValue(line);
+
+            switch (direction)
+            {
+                case "forward":
+                    horizontalPosition += value;
+                    break;
+
+                case "down":
+                    depth += value;
+                    break;
+
+                case "up":
+                    depth -= value;
+                    break;
+
+                default:
+                    throw new Exception($"Case {direction} not found!");
+            }
+        }
+
+        var result = horizontalPosition * depth;
+
+        PrettyConsole.Info($"Part1 result is {result}");
+    }
+
+    private static void ShowResultPart2(List<string> lines)
+    {
+        int horizontalPosition = 0;
+        int depth = 0;
+        int aim = 0;
+
+        foreach (var line in lines)
+        {
+            var direction = GetDirection(line);
+            var value = GetValue(line);
+
+            switch (direction)
+            {
+                case "forward":
+                    horizontalPosition += value;
+                    depth += aim * value;
+                    break;
+
+                case "down":
+                    aim += value;
+                    break;
+
+                case "up":
+                    aim -= value;
+                    break;
+
+                default:
+                    throw new Exception($"Case {GetDirection(line)} not found!");
+            }
+        }
+
+        var result = horizontalPosition * depth;
+
+        PrettyConsole.Info($"Part2 result is {result}");
+    }
+
+    private static int GetValue(string line)
+    {
+        return Convert.ToInt32(line.Split(' ')[1]);
+    }
+
+    private static string GetDirection(string line)
+    {
+        return line.Split(' ')[0];
+    }
+
+    private static List<string> GetInputLines(string fileName)
+    {
+        var inputReader = new InputReader(fileName);
+        return inputReader.ReadFile();
+    }
+}
---
+++ Shared/Helpers/ConsoleHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintHeader(int dayNum)
+        {
+            PrettyConsole.WriteWrappedHeader("Advent of code!", '*', headerColor: ConsoleColor.Green);
+            PrettyConsole.WriteWrappedHeader($"Day{dayNum}");
+        }
+
+        public static void Exit(bool isSucces = true, string? errorMessage = null)
+        {
+            if (isSucces)
+            {
+                PrettyConsole.WriteWrappedHeader("All completed. Quitting the application ...", headerColor: ConsoleColor.Green);
+
+                CloseConsole(0);
+            }
+
+            PrettyConsole.WriteWrappedHeader(
+                $"Somenthing went wrong. Quitting the application ... " +
+                (string.IsNullOrEmpty(errorMessage) ? string.Empty : "\nError message: " + errorMessage)
+                , headerColor: ConsoleColor.Red);
+
+            CloseConsole(-1);
+        }
+
+        private static void CloseConsole(int errorCode)
+        {
+            Console.WriteLine("Press any key to close the console ...");
+            Console.ReadKey();
+            Environment.Exit(errorCode);
+        }
+    }
+}
