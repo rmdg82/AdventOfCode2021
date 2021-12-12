dba7bb2528e134b6f926ccd8358693e4cbed6248
Romeo di Gregorio <r.digregorio@crif.com>
2021-12-12 16:01:03 +0100

    Add template for generic dayN

---
+++ Template/Program.cs
@@ -0,0 +1,35 @@
+﻿using Shared.Helpers;
+
+namespace $projectname$;
+
+public class Program
+{
+    private const string _inputFileName = "input1.txt";
+    private const string _testFileName = "testInput.txt";
+
+    public static void Main(string[] args)
+    {
+        ConsoleHelper.PrintHeader(dayNum: 2);
+
+        ShowResultPart1(GetInputLines(_inputFileName));
+        ShowResultPart2(GetInputLines(_inputFileName));
+
+        ConsoleHelper.Exit();
+    }
+
+    private static void ShowResultPart1(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void ShowResultPart2(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static List<string> GetInputLines(string fileName)
+    {
+        var inputReader = new InputReader(fileName);
+        return inputReader.ReadFile();
+    }
+}
